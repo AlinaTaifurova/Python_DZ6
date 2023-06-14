1c69ecc87f4dbf3582158bafc4482e462c6016a4
AlinaTaifurova <120791016+AlinaTaifurova@users.noreply.github.com>
2023-06-14 19:41:40 +0300

    Add files via upload

---
+++ # def num_in(list_1: list[int | float], .py
@@ -0,0 +1,9 @@
+
+a = int(input('Введите число первое '))
+d = int(input('Введите число первое '))
+n = int(input('Введите число первое '))
+list1 = []
+for i in range (n):
+    an = a + i * d
+    list1.append(an)
+print(list1)
